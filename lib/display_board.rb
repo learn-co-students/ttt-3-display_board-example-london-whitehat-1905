9f8f30f3c627a8749eb32a4a033a4cc2ce0f5cf8
NJ-JayJay <metalmedley1@gmail.com>
2019-05-01 14:14:45 +0100

    Ugly implementation

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,14 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board()
+    board = [" ", " ", " "," ", " ", " "," ", " ", " "]
+    for i in (0...9)
+      if (i+1) % 3 == 0 and (i+1) != 9
+        print " " + board[i] + " "
+        print "\n" + "-"*11 + "\n"
+      elsif (i+1) == 9
+        print " " + board[i] + " \n"
+      else
+        print " " + board[i] + " |"
+      end
+    end
+end
